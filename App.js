3b6d85aaea466b16db4496381d72228405ba346a
Temkhu <teemu.ritatorma@gmail.com>
2024-02-12 15:00:18 +0200

    j

--- App.js
+++ App.js
@@ -1,12 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import Map from './screens/Map';
+import Constants from 'expo-constants';
+import { useState } from 'react';
+import * as Location from 'expo-location'
+import { PaperProvider } from 'react-native-paper';
+import MainAppBar from './components/MainAppBar';
+
+const settings = {
+  backgroundColor: '#00a484'
+}
+
+const icons = {
+  location_not_known: 'crosshairs',
+  location_searching: 'crosshairs-question',
+  location_found: 'crosshairs-gps'
+}
 
 export default function App() {
+
+const [icon, setIcon] = useState(icons.location_not_known)
+const[location, setLocation] = useState({
+  latitude: 65.0800,
+  longitude: 25.4800,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+})
+
+const GetUserLocation = async () =>{
+
+  let {status} = await Location.requestForegroundPermissionsAsync()
+  
+  try {
+      if(status !== 'granted'){
+          console.log('Geolocation failed')
+          return
+      }
+      const position = await Location.getCurrentPositionAsync({accuracy: Location.Accuracy.High})
+      setLocation({...location,"latitude" : position.coords.latitude, "longitude" : position.coords.longitude})
+  } catch (error) {
+      console.log(error)
+  }
+
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+<PaperProvider>
+  <MainAppBar 
+  title="Map"
+  backgroundColor={settings.backgroundColor}
+  icon={icon}
+  GetUserLocation={GetUserLocation}
+  />
+  <SafeAreaView style={styles.container}>
+    <Map location={location}/>
+  </SafeAreaView>
+  </PaperProvider>
   );
 }
 
@@ -16,5 +65,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? Constants.statusBarHeight: 0
   },
 });
---
+++ screens/Map.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import * as Location from 'expo-location'
+
+export default function Map(props) {
+const [pin, setPin] = useState(null)
+
+
+/*useEffect(() => {
+  (async()=> {
+    GetUserLocation()
+  })()
+}, [])*/
+
+const showPin = (e) => {
+    const coords = e.nativeEvent.coordinate
+    setPin(coords)
+}
+
+  return (
+   <MapView
+   style={styles.map}
+   region={props.location}
+   mapType='standard'
+   userInterfaceStyle='default'
+   onLongPress={showPin}
+   showsUserLocation={true}
+   >
+    {pin &&
+    <Marker 
+    title='Paikka'
+    coordinate={{latitude: pin.latitude, longitude: pin.longitude}}
+    />
+    }
+   </MapView>
+
+  )
+}
+
+const styles = StyleSheet.create({
+
+    map: {
+        height: '100%',
+        width: '100%'
+    }
+});
